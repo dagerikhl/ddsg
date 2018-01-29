e60bfad532fa2d5d32038efee02bc7fc3f8e3058
Dag Erik <dagerikhl@gmail.com>
2018-01-29 17:10:54 +0100

    Create simple dummy-logic for Ground and Board

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace DdSG {
+
+    public class BoardController : MonoBehaviour {
+
+        public GameObject Ground;
+
+        private void Start() {
+        }
+
+        private void Update() {
+        }
+
+    }
+
+}
