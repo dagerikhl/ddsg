2fadedfd2f63f8e30d0ecac7d87070863a9af947
Dag Erik <dagerikhl@gmail.com>
2018-04-16 23:41:12 +0200

    Add more finesse to options UI

--- Assets/Scripts/UserInterface/OptionsMenu.cs
+++ Assets/Scripts/UserInterface/OptionsMenu.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace DdSG {
 
@@ -8,6 +9,9 @@ namespace DdSG {
 
         [Header("Unity Setup Fields")]
         public SceneManager SceneManager;
+        public Slider VolumeSlider;
+        public Toggle MusicToggle;
+        public Toggle SoundsToggle;
 
         //[Header("Optional")]
 
@@ -15,6 +19,46 @@ namespace DdSG {
         //[HideInInspector]
 
         // Private members
+
+        public void SetVolume(Slider volumeSlider) {
+            // Set volume
+            AudioListener.volume = volumeSlider.value/10f;
+
+            // Update UI
+            if (volumeSlider.value <= 0) {
+                MusicToggle.interactable = false;
+                SoundsToggle.interactable = false;
+            } else {
+                MusicToggle.interactable = true;
+                SoundsToggle.interactable = true;
+            }
+        }
+
+        public void ToggleMusic(Toggle musicToggle) {
+            // Set volume
+            // TODO Distinguish between music and sounds
+            AudioListener.volume = 0;
+
+            // Update UI
+            if (!musicToggle.isOn && !SoundsToggle.isOn) {
+                VolumeSlider.interactable = false;
+            } else {
+                VolumeSlider.interactable = true;
+            }
+        }
+
+        public void ToggleSounds(Toggle soundsToggle) {
+            // Set volume
+            // TODO Distinguish between music and sounds
+            AudioListener.volume = 0;
+
+            // Update UI
+            if (!soundsToggle.isOn && !MusicToggle.isOn) {
+                VolumeSlider.interactable = false;
+            } else {
+                VolumeSlider.interactable = true;
+            }
+        }
 
         public void GoToLastMenu() {
             SceneManager.GoTo(State.LastScene);
