dc9865b3750f5f7e006f57628f82f6ce2c27b026
Dag Erik <dagerikhl@gmail.com>
2018-04-17 16:29:54 +0200

    Add level select menu and highscore menu script

---
+++ Assets/Scripts/UserInterface/HighscoreMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace DdSG {
+    public class HighscoreMenu: MonoBehaviour {
+
+        //[Header("Attributes")]
+
+        //[Header("Unity Setup Fields")]
+
+        //[Header("Optional")]
+
+        // Public members hidden from Unity Inspector
+        //[HideInInspector]
+
+        // Private members
+
+    }
+}
---
+++ Assets/Scripts/UserInterface/LevelSelectMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace DdSG {
+
+    public class LevelSelectMenu: MonoBehaviour {
+
+        //[Header("Attributes")]
+
+        //[Header("Unity Setup Fields")]
+
+        //[Header("Optional")]
+
+        // Public members hidden from Unity Inspector
+        //[HideInInspector]
+
+        // Private members
+
+    }
+
+}
