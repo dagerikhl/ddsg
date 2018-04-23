8dccec9c610cfb3a7c9d54ad9304210dfc2b7b49
Dag Erik <dagerikhl@gmail.com>
2018-04-23 15:38:25 +0200

    Add dropdown behaviour to ui behaviour

---
+++ Assets/Scripts/UserInterface/Behaviours/DropdownBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.UI;
+
+namespace DdSG {
+
+    public class DropdownBehaviour: ClickBehaviour<Dropdown> {
+
+        //[Header("Attributes")]
+
+        //[Header("Unity Setup Fields")]
+
+        //[Header("Optional")]
+
+        // Public members hidden from Unity Inspector
+        //[HideInInspector]
+
+        // Private members
+
+        private void Start() {
+            clickable.onValueChanged.AddListener((value) => PerformClickBehaviour());
+        }
+
+    }
+
+}
