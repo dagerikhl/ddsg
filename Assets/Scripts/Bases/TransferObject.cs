828d614300caea57cd97852fbf0a1b171e2fe71f
Dag Erik <dagerikhl@gmail.com>
2018-05-04 23:25:06 +0200

    Change from self-written transfer object print to json prettyprint

--- Assets/Scripts/Bases/TransferObject.cs
+++ Assets/Scripts/Bases/TransferObject.cs
@@ -1,49 +1,13 @@
 ﻿using System;
-using System.Collections;
-using System.Reflection;
-using System.Text;
+using UnityEngine;
 
 namespace DdSG {
 
     [Serializable]
     public abstract class TransferObject {
 
-        private static int depth;
-
         public override string ToString() {
-            FieldInfo[] fieldInfos = GetType().GetFields();
-
-            var sb = new StringBuilder();
-
-            if (fieldInfos.Length > 0) {
-                sb.AppendLine("{");
-                depth++;
-            }
-            foreach (var info in fieldInfos) {
-                var value = info.GetValue(this) ?? "(null)";
-
-                if (value.GetType().IsArray) {
-                    sb.AppendLine(indent() + info.Name + ": [");
-                    depth++;
-                    foreach (var element in (IEnumerable) value) {
-                        sb.AppendLine(indent() + element);
-                    }
-                    depth--;
-                    sb.AppendLine(indent() + "]");
-                } else {
-                    sb.AppendLine(indent() + info.Name + ": " + value);
-                }
-            }
-            if (fieldInfos.Length > 0) {
-                depth--;
-                sb.AppendLine(indent() + "}");
-            }
-
-            return sb.ToString();
-        }
-
-        private string indent() {
-            return ' '.Repeat(depth*2);
+            return JsonUtility.ToJson(this, true);
         }
 
     }
