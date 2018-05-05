bbdc65d0450b6963a2fc0d791afa9ee6804cbbc8
Dag Erik <dagerikhl@gmail.com>
2018-05-05 15:03:10 +0200

    Start on nested types for attack pattern

--- Assets/Scripts/Models/Entities/AttackPattern.cs
+++ Assets/Scripts/Models/Entities/AttackPattern.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
+
+// ReSharper disable InconsistentNaming
 
 namespace DdSG {
 
     [Serializable]
-    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public class AttackPattern: StixDataEntityBase {
 
         public AttackPatternCustoms custom;
@@ -12,8 +12,45 @@ namespace DdSG {
     }
 
     [Serializable]
-    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public class AttackPatternCustoms: TransferObjectBase {
+
+        public AttackSteps steps;
+        public string[] prerequisites;
+        public string severity;
+        public string likelihood;
+        public AttackExample[] examples;
+        public string[] probing_techniques;
+        public string[] indicators;
+        public AttackMotivation[] motivations;
+        public AttackInjectionVector injection_vector;
+        public string payload;
+        public string activation_zone;
+        public AttackImpact impact;
+
+    }
+
+    [Serializable]
+    public class AttackSteps {
+
+    }
+
+    [Serializable]
+    public class AttackExample {
+
+    }
+
+    [Serializable]
+    public class AttackMotivation {
+
+    }
+
+    [Serializable]
+    public class AttackInjectionVector {
+
+    }
+
+    [Serializable]
+    public class AttackImpact {
 
     }
 
