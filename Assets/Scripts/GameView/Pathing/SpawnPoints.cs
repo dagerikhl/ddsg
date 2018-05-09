bcc49ea277511218bfc65cf89efac6d3fb01ebf3
Dag Erik <dagerikhl@gmail.com>
2018-05-09 12:14:00 +0200

    Add spawn points

---
+++ Assets/Scripts/GameView/Pathing/SpawnPoints.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace DdSG {
+
+    public class SpawnPoints: MonoBehaviour {
+
+        //[Header("Attributes")]
+
+        [Header("Unity Setup Fields")]
+        public Transform ServerSpawnPoint;
+        public Transform NetworkSpawnPoint;
+        public Transform ClientSpawnPoint;
+
+        //[Header("Optional")]
+
+        // Public members hidden from Unity Inspector
+
+        // Private members
+
+    }
+
+}
