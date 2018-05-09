03309e6b493407a067907ce363a07b3c66ff96b5
Dag Erik <dagerikhl@gmail.com>
2018-05-09 17:10:22 +0200

    Add sockets (spawning points) for assets
    Limit to 4 for now.

---
+++ Assets/Scripts/GameView/Spawning/AssetSockets.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace DdSG {
+
+    public class AssetSockets: MonoBehaviour {
+
+        //[Header("Attributes")]
+
+        [Header("Unity Setup Fields")]
+        public Transform AssetSocket1;
+        public Transform AssetSocket2;
+        public Transform AssetSocket3;
+        public Transform AssetSocket4;
+
+        //[Header("Optional")]
+
+        // Public members hidden from Unity Inspector
+        public static Transform[] Sockets;
+
+        // Private members
+
+        private void Awake() {
+            Sockets = new Transform[] { AssetSocket1, AssetSocket2, AssetSocket3, AssetSocket4 };
+        }
+
+    }
+
+}
