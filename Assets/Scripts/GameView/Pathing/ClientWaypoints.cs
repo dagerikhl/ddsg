495df82472abbe80adc2faa97745939d41502f38
Dag Erik <dagerikhl@gmail.com>
2018-05-09 11:55:31 +0200

    Add waypoints and paths

---
+++ Assets/Scripts/GameView/Pathing/ClientWaypoints.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace DdSG {
+
+    public class ClientWaypoints: MonoBehaviour {
+
+        //[Header("Attributes")]
+
+        //[Header("Unity Setup Fields")]
+
+        //[Header("Optional")]
+
+        // Public members hidden from Unity Inspector
+        public static Transform[] Points;
+
+        // Private members
+
+        private void Awake() {
+            Points = new Transform[transform.childCount];
+            for (var i = 0; i < Points.Length; i++) {
+                Points[i] = transform.GetChild(i);
+            }
+        }
+
+    }
+
+}
