bf2c84dfd1e38e3acc4b5f9ca90fb239444817ef
Dag Erik <dagerikhl@gmail.com>
2018-05-10 12:34:26 +0200

    Bind gametime to update

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace DdSG {
+
+    public class GameManager: MonoBehaviour {
+
+        //[Header("Attributes")]
+
+        [Header("Unity Setup Fields")]
+        public TextMeshProUGUI GameTime;
+
+        //[Header("Optional")]
+
+        // Public members hidden from Unity Inspector
+
+        // Private members
+        private float secondsElapsed;
+
+        private void Update() {
+            secondsElapsed += Time.deltaTime;
+
+            GameTime.text = TimeHelper.FormatTime(secondsElapsed);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Utilities/TimeHelper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace DdSG {
+
+    public static class TimeHelper {
+
+        public static string FormatTime(float seconds) {
+            var minutes = Mathf.RoundToInt(seconds/60);
+            var secondsRemaining = Mathf.RoundToInt(seconds%60);
+            Logger.Debug(minutes);
+            Logger.Debug(secondsRemaining);
+            Logger.Debug(seconds);
+
+            return string.Format("{0}:{1}", minutes, secondsRemaining);
+        }
+
+    }
+
+}
