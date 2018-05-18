c471ddd2b1ff9e06fc8b3ba05a03dd10f9577358
Dag Erik <dagerikhl@gmail.com>
2018-05-18 15:48:28 +0200

    Add attributes in hover menu for attack patterns

--- Assets/Scripts/Utilities/Formatter.cs
+++ Assets/Scripts/Utilities/Formatter.cs
@@ -28,15 +28,44 @@ namespace DdSG {
             return waveString.PadLeft(2);
         }
 
-        // TODO Create a custom one for attack patterns to show more custom data
         public static string BuildStixDataEntityDescription(StixDataEntityBase entity) {
             var sb = new StringBuilder();
 
             sb.AppendLine(entity.FullDescription);
             sb.AppendLine();
-            sb.Append("<i>Sources</i>");
+
+            if (entity.GetType() == typeof(AttackPattern)) {
+                var attackPattern = (AttackPattern) entity;
+                sb.AppendLine("<i><size=140%>Attributes</size></i>");
+                sb.AppendLine();
+                if (attackPattern.custom.severity != null) {
+                    sb.AppendLine(
+                        BuildAttributeText(
+                            "Severity",
+                            EnumHelper.GetEnumMemberAttributeValue((Scale) attackPattern.custom.severity)));
+                }
+                if (attackPattern.custom.likelihood != null) {
+                    sb.AppendLine(
+                        BuildAttributeText(
+                            "Likelihood",
+                            EnumHelper.GetEnumMemberAttributeValue((Scale) attackPattern.custom.likelihood)));
+                }
+                sb.AppendLine(
+                    BuildAttributeText(
+                        "Injection Vector",
+                        BuildInjectionVectorText(attackPattern.custom.injection_vector)));
+                sb.AppendLine(BuildAttributeText("Payload", attackPattern.custom.payload));
+                sb.AppendLine(
+                    BuildAttributeText(
+                        "Activation Zone",
+                        BuildActivationZoneText(attackPattern.custom.activation_zone)));
+                sb.AppendLine(BuildAttributeText("Impact", BuildImpactText(attackPattern.custom.impact)));
+                sb.AppendLine("<indent=0>");
+            }
+
+            sb.Append("<i><size=140%>Sources</size></i>");
             if (entity.external_references.Any((e) => e.description == null)) {
-                sb.AppendLine("<space=30em><color=#848484><b>Right-click to open all in browser</b></color>");
+                sb.AppendLine("<space=25em><color=#848484><b>Right-click to open all in browser</b></color>");
             } else {
                 sb.AppendLine();
             }
@@ -44,13 +73,13 @@ namespace DdSG {
             foreach (var externalReference in entity.external_references) {
                 if (externalReference.description == null) {
                     sb.AppendFormat(
-                        "<i><b><indent=2em>\U00002014<indent=7em>{0}</b> (from <color=#96c8e9>{1}</color>)</i>",
+                        "<indent=2em>\U00002014<indent=7em><b>{0}</b> (from <color=#96c8e9>{1}</color>)",
                         externalReference.id,
                         externalReference.url.ExtractUrlDomain());
                 } else {
                     sb.AppendFormat("<i><b><indent=7em>{0}</b></i>", externalReference.description);
                 }
-                sb.AppendLine();
+                sb.AppendLine("<indent=0>");
             }
 
             return sb.ToString();
@@ -58,6 +87,29 @@ namespace DdSG {
 
         public static string BuildActionText(string text) {
             return !string.IsNullOrEmpty(text) ? "Click to " + text : "";
+        }
+
+        private static string BuildAttributeText(string label, string text) {
+            return string.Format(
+                "<indent=2em>\U00002014<indent=7em><b>{0}:</b> {1}",
+                label,
+                !string.IsNullOrEmpty(text) ? text : "-");
+        }
+
+        private static string BuildInjectionVectorText(AttackInjectionVector vector) {
+            return string.Format("{0} <i>({1})</i>", vector.description, vector.categories.Join(", "));
+        }
+
+        private static string BuildActivationZoneText(AttackActivationZone zone) {
+            return string.Format("{0} <i>({1})</i>", zone.description, zone.categories.Join(", "));
+        }
+
+        private static string BuildImpactText(AttackImpact impact) {
+            return string.Format(
+                "Confidentiality: {0}, Integrity: {1}, Availability: {2}",
+                impact.confidentiality,
+                impact.integrity,
+                impact.availability);
         }
 
     }
