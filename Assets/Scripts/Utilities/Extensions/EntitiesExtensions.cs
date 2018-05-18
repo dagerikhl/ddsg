dbfe37417c18c56b7352509e9aed11f52fdd1b04
Dag Erik <dagerikhl@gmail.com>
2018-05-18 10:30:08 +0200

    Move model helper method extensions out of model

---
+++ Assets/Scripts/Utilities/Extensions/EntitiesExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace DdSG {
+
+    public static class EntitiesExtensions {
+
+        public static float calculateHealthFromSeverity(this AttackPattern attackPattern, float baseValue) {
+            return baseValue + attackPattern.custom.severity.AsFloat()*baseValue;
+        }
+
+        public static float calculateSpawnLikelihoodFromLikelihood(this AttackPattern attackPattern, float baseValue) {
+            if (attackPattern.custom.likelihood == null) {
+                return baseValue;
+            }
+
+            var likelihoodPart = attackPattern.custom.likelihood.AsPart();
+            return MathHelper.Rangify(likelihoodPart, 0.5f, 1f);
+        }
+
+        public static float calculateDamageToAssetFromImpact(this AttackPattern attackPattern, float baseValue) {
+            if (attackPattern.custom.impact == null) {
+                return baseValue;
+            }
+
+            var averageScale = (attackPattern.custom.impact.availability.AsPart()
+                                + attackPattern.custom.impact.confidentiality.AsPart()
+                                + attackPattern.custom.impact.integrity.AsPart())
+                               /3f;
+
+            return baseValue + averageScale*9f;
+        }
+
+    }
+
+}
